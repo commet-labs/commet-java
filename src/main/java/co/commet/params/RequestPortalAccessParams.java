22893d6321f1e41d155e2d9c6aeded71bef0264e
Franco Jalil <franco@commet.co>
2026-07-08 16:37:26 -0300

    feat: support portal return urls

--- src/main/java/co/commet/params/RequestPortalAccessParams.java
+++ src/main/java/co/commet/params/RequestPortalAccessParams.java
@@ -4,11 +4,13 @@ public final class RequestPortalAccessParams {
 
     private final String email;
     private final String customerId;
+    private final String returnUrl;
     private final String idempotencyKey;
 
     private RequestPortalAccessParams(Builder builder) {
         this.email = builder.email;
         this.customerId = builder.customerId;
+        this.returnUrl = builder.returnUrl;
         this.idempotencyKey = builder.idempotencyKey;
     }
 
@@ -18,12 +20,14 @@ public final class RequestPortalAccessParams {
 
     public String getEmail() { return email; }
     public String getCustomerId() { return customerId; }
+    public String getReturnUrl() { return returnUrl; }
     public String getIdempotencyKey() { return idempotencyKey; }
 
     public static final class Builder {
 
         private String email;
         private String customerId;
+        private String returnUrl;
         private String idempotencyKey;
 
         private Builder() {
@@ -36,6 +40,11 @@ public final class RequestPortalAccessParams {
 
         public Builder customerId(String customerId) {
             this.customerId = customerId;
+            return this;
+        }
+
+        public Builder returnUrl(String returnUrl) {
+            this.returnUrl = returnUrl;
             return this;
         }
 
